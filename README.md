74f652d3e4c1be5aab4713b5d8488fda67669059
rubycs <rubycs@clair.ro>
2011-03-14 19:54:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# CSS documentation generator
+
+A small Ruby executable (shell) script to generate HTML documentation for CSS files.
+
+More exactly:
+
+  1. It takes a .css file
+  2. Takes all comments in Markdown format and generates a HTML
+  
+## Why?
+
+* There are no proper CSS documenting tools.
+* It is more straightforward to document your frontend code in CSS than in HTML
+
+## Based on
+
+  1. rdiscount
+  
+## Usage
+
+Comment your CSS as usual but use [Markdown](http://daringfireball.net/projects/markdown/syntax ) instead of plain text.
+
+Example:
+  
+    /*
+    documentation line 1
+    documentation line 2
+    */ 
+    
+Then run `cssd` on your CSS file. It will generate a HTML file named as your CSS.
+
+## Licence
+
+MIT
---
+++ cssd.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+# cssd.rb
+# - extracting documentation from CSS files and converting to markdown
+#
+# - documentation text must be put on separate lines
+# - example:
+#   
+#   /*
+#   documentation line #1
+#   documentation line #2
+#   */
+#
+# - the documentation must use the Gruber markdown format: http://daringfireball.net/projects/markdown/syntax
+
+
+
+require 'rdiscount'
+
+unless ARGV.empty? 
+  arg = ARGV[0]
+  
+  source = "#{arg}.css"
+  if File.exists? source    
+    dest = "#{arg}.html"
+  
+    ret = ''
+    record = false
+    File.open(source, 'r') do |f|
+      f.each_line do |line|
+        record = true if line.include? "/*"
+        record = false if line.include? "*/"    
+        next if (line.include? "/*") || (line.include? "*/")
+        ret += line if record && line != ''                
+      end
+    end
+    
+    markdown = RDiscount.new(ret)
+    File.open(dest, 'w') { |f| f.write(markdown.to_html) } 
+    
+    puts "#{dest} created sucessfuly."
+  else
+    puts "File does not exists: #{source}" 
+  end
+else
+  puts "Generate documentation from CSS through Markdown into HTML." 
+  puts " - Usage: cssd filename (without extension, .css is used automaticaly)"
+  puts " - Input files are taken from /assets"
+  puts " - Output files are put into /doc"
+end
